2ad561c84c7a7bfe805209a06f993b4a6fed1151
China-Liyq <908337205@qq.com>
2021-04-15 23:11:10 +0800

    Initial commit

---
+++ src/main/java/com/chinaliyq/spring/bean/com/chinaliyq/spring/App.java
@@ -0,0 +1,18 @@
+package com.chinaliyq.spring.bean.com.chinaliyq.spring;
+
+import com.chinaliyq.spring.bean.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: liyq
+ * @Description: spring_maven
+ * @Date: 2021/4/15 - 22:56
+ * @Version: 1.0
+ **/
+public class App {
+    public static void main(String[] args) {
+        final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application.xml");
+        final Person xy = ctx.getBean("xy", Person.class);
+        System.out.println(xy);
+    }
+}
